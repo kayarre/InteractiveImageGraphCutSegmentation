80530e85d03aefc1079d43cb46dc278c7decba41
Kurt Sansom <kayarre@gmail.com>
2016-03-09 11:31:05 -0800

    update for qt5

--- GraphCutSegmentationWidget.cpp
+++ GraphCutSegmentationWidget.cpp
@@ -54,7 +54,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QFileDialog>
 #include <QLineEdit>
 #include <QMessageBox>
-#include <QtConcurrentRun>
+#include <QtConcurrent/QtConcurrent>
 
 // STL
 #include <iostream>
